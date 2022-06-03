a0d97de7fd84133bf22e00828060a6aa05a517bd
Amanda Estrela <efernandes.amanda@gmail.com>
2022-06-02 21:39:44 -0300

    Exercicio Contador - cor vermelha para números negativos

--- contador/assets/js/main.js
+++ contador/assets/js/main.js
@@ -1,16 +1,21 @@
 const add = document.getElementById('increment');
 const remove = document.getElementById('decrement');
-const contador = document.getElementById('contador')
+const contador = document.getElementById('contador');
 
 add.addEventListener("click",somaContador);
 remove.addEventListener("click",subtraiContador);
 
 function somaContador(){
     contador.innerHTML = parseInt(contador.innerHTML) +1;
+    if(parseInt(contador.innerHTML) >= 0){
+        contador.style.color = "#FF5D5D";
+    }
 }
 
 function subtraiContador() {
     contador.innerHTML = parseInt(contador.innerHTML) -1;
+    if(parseInt(contador.innerHTML) < 0){
+        contador.style.color = "red";
+    } 
 }
-
 
